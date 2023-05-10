80f090cc405e2c74dc51b6668bc7063b93d03235
Jeff Owuor <99865024+Jeff-Owuor@users.noreply.github.com>
2023-05-10 22:54:15 +0300

    Handle toggle functionality using state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { useState } from 'react';
 import './App.css';
 import BlogEditor from './BlogEditor';
 
 function App() {
+  const [darkMode,setDarkMode] = useState(false)
+  const handleToggleMode = () => {
+    setDarkMode(!darkMode);
+  };
   return (
-    <div className="App">
+    <div className={`App ${darkMode ? 'dark' : 'light'}`}>
+        <button onClick={handleToggleMode}><i className="fas fa-adjust fa-2x modeButton"></i></button>
          <BlogEditor/>
     </div>
   );
